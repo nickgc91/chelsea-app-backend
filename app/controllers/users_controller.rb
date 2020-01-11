155c500ed1b80122ebdd34341f2d70fa63424d61
Nicolas Charlet <nicolas@nicolascharlet.com>
2020-01-11 20:34:29 +0000

    creating auth on the backend to allow users to login and have a profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
 
+
+    def signin
+        user = User.find_by(username: params[:username])
+
+        if user && user.authenticate(params[:password])
+            #found user
+            render json: user
+        else
+            #didn't find user 
+            render json: { error: 'Error - username/password combination invalid' }, status: 401 
+        end
+
+    end
+
+
     def index
         users = User.all
         render json: users
     end
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_12_10_171846) do
     t.string "first_name"
     t.string "last_name"
     t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
