fb657d13701d5d15044022ee168c33848bfb43f8
Nicolas Charlet <nicolas@nicolascharlet.com>
2020-01-21 17:09:02 +0000

    setting up jwt to check token validity

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
         user = User.find_by(username: sign_in_params[:username])
         if user && user.authenticate(sign_in_params[:password])
             #found user
-            render json: { username: user.username, id: user.id }
+            render json: { username: user.username, token: issue_token({ id: user.id }) }
         else
             #didn't find user 
             render json: { error: 'Username/password combination invalid' }, status: 401 
@@ -13,16 +13,23 @@ class UsersController < ApplicationController
     end
 
     def validate
-        id = request.headers['Authorization'].to_i
-        user = User.find_by(id: id)
+         user = get_current_user
         if user
-            render json: user
+            render json: { username: user.username, token: issue_token({ id: user.id }) } 
         else
             render json: {error: 'Invalid token.'}, status: 401
         end
-
     end
 
+
+    def profile
+        user = get_current_user
+        if user
+            render json: { first_name: user.first_name, last_name: user.last_name } 
+        else
+            render json: {error: 'Invalid token.'}, status: 401
+        end
+    end
 
 
     def index
