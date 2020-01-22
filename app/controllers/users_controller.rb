130a3c0dbe97e88337b020773e67cd6dfb850e96
Nicolas Charlet <nicolas@nicolascharlet.com>
2020-01-22 14:12:23 +0000

    Created seed data and rendering personalized profile data for each user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
     def profile
         user = get_current_user
         if user
-            render json: { first_name: user.first_name, last_name: user.last_name } 
+            render json: { first_name: user.first_name, last_name: user.last_name, favorite_player: user.favorite_player } 
         else
             render json: {error: 'Invalid token.'}, status: 401
         end
--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_12_10_171846) do
     t.string "last_name"
     t.string "username"
     t.string "password_digest"
+    t.string "favorite_player"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
